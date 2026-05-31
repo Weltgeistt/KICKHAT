f771a3a4d4488b492199702d25589ada28c014dc
Poyland <aynen3132@gmail.com>
2026-06-01 00:14:42 +0300

    Fix pusher-js require for Node environment

--- kickhat-bot/index.js
+++ kickhat-bot/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const Pusher = require('pusher-js');
+const { Pusher } = require('pusher-js');
 const { initDB, saveMessage, markMessageDeleted, getUserMessages } = require('./db');
 
 const app = express();
